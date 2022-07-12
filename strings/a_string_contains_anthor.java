e2514f4b7b1bb515b0e962e071cfddca5a7c98cf
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-07-12 22:42:11 +0200

    Create a_string_contains_anthor.java
    التحقق من إن كان سترينج يحتوي على سترينج اخر 
    checking if a string contains anthor

---
+++ strings/a_string_contains_anthor.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class main {
+	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+
+		String s1 = input.next();
+		String s2 = input.next();
+
+	if(s1.contains(s2))
+	System.out.println("true");
+  else 
+  System.out.println("false"); 
+				}
+			}
