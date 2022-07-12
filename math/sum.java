0b2e68e05df025b3a7c695422ffe53976d343621
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-07-12 23:19:41 +0200

    Create sum.java

---
+++ math/sum.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static int summation(int n) {
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+        sum += i;
+
+        }
+    return sum;
+    }
+}
