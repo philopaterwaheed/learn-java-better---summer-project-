0c82cdd86b6ab602d58620a1d2e7a126f73b6a8d
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-07-12 22:54:28 +0200

    check_if_a_string_contains_anthor

--- strings/a_string_contains_anthor.java
+++ strings/a_string_contains_anthor.java
@@ -6,7 +6,7 @@ public class main {
 		String s1 = input.next();
 		String s2 = input.next();
 
-	if(s1.contains(s2))
+	if(s1.contains(s2)) // check if the string contains anthor throw a bulid in function 
 	System.out.println("true");
   else 
   System.out.println("false"); 
